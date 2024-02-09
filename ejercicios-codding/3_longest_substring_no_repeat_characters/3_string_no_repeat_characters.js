bbcfa37bca26fd82facf84de6ab2319ca4dfa4ff
Nelson Venegas <nelson.venegasg@gmail.com>
2024-02-09 00:43:20 -0300

    feat: add problem 3 "no_repeat_string"

---
+++ ejercicios-codding/3_longest_substring_no_repeat_characters/3_string_no_repeat_characters.js
@@ -0,0 +1,34 @@
+function lengthOfLongestSubstring(s) {
+    var toArray = s.split('');
+    var results = {
+        sum: 0,
+        substring: [],
+        acum: []
+    };
+    for (var _i = 0, toArray_1 = toArray; _i < toArray_1.length; _i++) {
+        var letter = toArray_1[_i];
+        if (!results.substring.includes(letter)) {
+            results.sum += 1;
+            results.substring.push(letter);
+        }
+        else if (results.substring.includes(letter)) {
+            results.acum.push(results.sum);
+            results.substring.push(letter);
+            results.sum -= 1;
+        }
+        else {
+            results.sum = 0;
+        }
+    }
+    return Math.max.apply(Math, results.acum);
+}
+var test_1 = lengthOfLongestSubstring('abcabcbb');
+var test_2 = lengthOfLongestSubstring('bbbbb');
+var test_3 = lengthOfLongestSubstring('pwwkew');
+var test_4 = lengthOfLongestSubstring('rtesessadqwer');
+console.table({
+    test_1: test_1,
+    test_2: test_2,
+    test_3: test_3,
+    test_4: test_4
+});
---
+++ ejercicios-codding/3_longest_substring_no_repeat_characters/3_string_no_repeat_characters.ts
@@ -0,0 +1,42 @@
+interface resultsProps {
+  sum: number
+  substring: string[]
+  acum: number[]
+}
+
+function lengthOfLongestSubstring(s: string): number {
+  const toArray = s.split('')
+
+  let results: resultsProps = {
+    sum: 0,
+    substring: [],
+    acum: []
+  }
+
+  for (let letter of toArray) {
+    if (!results.substring.includes(letter)) {
+      results.sum += 1
+      results.substring.push(letter)
+    } else if (results.substring.includes(letter)) {
+      results.acum.push(results.sum)
+      results.substring.push(letter)
+      results.sum -= 1
+    } else {
+      results.sum = 0
+    }
+  }
+
+  return Math.max(...results.acum)
+}
+
+const test_1 = lengthOfLongestSubstring('abcabcbb')
+const test_2 = lengthOfLongestSubstring('bbbbb')
+const test_3 = lengthOfLongestSubstring('pwwkew')
+const test_4 = lengthOfLongestSubstring('rtesessadqwer')
+
+console.table({
+  test_1,
+  test_2,
+  test_3,
+  test_4
+})
